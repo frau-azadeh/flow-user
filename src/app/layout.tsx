d90c2c0162fbd05c52cdb11e8cf62fe868e7ce02
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-03 01:56:39 -0800

    use ReactQueryProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactQueryProvider } from "@/lib/react-query";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <ReactQueryProvider>
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
