c1b7d44794dc6f7caa0dcbef5d9f0ee072d0e0d3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-06 04:11:50 -0800

    update

--- src/lib/api/employee.ts
+++ src/lib/api/employee.ts
@@ -1,11 +1,21 @@
 import { api } from "../api";
 
-export const createEmployee = async (employee:{name: string; email:string; position:string})=>{
+export const createUser = async (employee:{name: string; email:string; position:string})=>{
     const {data} = await api.post("/", employee);
     return data;
 }
 
-export const getEmployees = async () => {
+export const getUsers = async () => {
     const { data } = await api.get("/");
     return data;
   };
+
+  export const updateUser = async (id: string, user: { name: string; email: string }) => {
+    const { data } = await api.put(`/users/${id}`, user); 
+    return data;
+  };
+  
+  export const deleteUser = async (id: string) => {
+    await api.delete(`/users/${id}`); 
+  };
+  
