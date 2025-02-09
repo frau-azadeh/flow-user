f50a0b8d87c234271ba6302d1aa35226eda00b88
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-09 05:06:18 -0800

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://679a1892747b09cdcccdac6b.mockapi.io/employee/managment"
+const API_BASE_URL ="https://679a1892747b09cdcccdac6b.mockapi.io/employee/managment"
 
 export const api = axios.create({
-    baseURL:  API_BASE_URL,
+    baseURL: API_BASE_URL,
     headers:{
         "Content-Type": "aplication/json",
     }
