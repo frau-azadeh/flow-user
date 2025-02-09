54931e0f4e3ebe0d092cb4ac6ca534fde03ad074
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-02-09 05:09:41 -0800

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_BASE_URL ="https://679a1892747b09cdcccdac6b.mockapi.io/employee/managment"
+const API_BASE_URL ="https://679a1892747b09cdcccdac6b.mockapi.io/employee/managment";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
-    headers:{
-        "Content-Type": "aplication/json",
-    }
+    headers: {
+        "Content-Type": "application/json"
+    },
 })
